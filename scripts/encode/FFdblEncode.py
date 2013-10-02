20b465280f057318350f6e17a4f7d9efbd13b97d
Martin Hosken <martin_hosken@sil.org>
2013-10-02 23:10:42 +0700

    Make better use of core libraries, particularly os.path
    This initial change tidies up some python coding style to make better use of existing python libraries and functions. Allowing for more generic coding. No functionality change here (except closing the control file slightly later).

--- scripts/encode/FFdblEncode.py
+++ scripts/encode/FFdblEncode.py
@@ -9,26 +9,16 @@
 # Copyright (c) 2013, SIL International  (http://www.sil.org)
 # Released under the MIT License (http://sil.mit-license.org)
 
-import fontforge, sys, string
+import fontforge, sys, string, os.path
 
 def splitfn(fn): # Split filename into path, base and extension
-	fsplit = fn.split("/")
-	fpath=""
-	j=len(fsplit)-1
-	for i in range(0,j):
-		fpath = fpath + fsplit[i] + '/'
-	fn = fsplit[j] # Now just has base + extension
-	fsplit = fn.split(".")
-	fbase = fsplit[0] # Assume no .'s in base
-	if len(fsplit) == 1:
-		fext=""
-	else:
-		fext = "." + fsplit[1]
-	return (fpath,fbase,fext)
+	p = os.path.split(fn)
+	f = os.path.splitext(p[1])
+	return (p[0], f[0], os.path.extsep + f[1])
 
 def UniStr(u):
 	if u:
-		return "U+" + string.zfill(string.upper(hex(u)[2:]), 4)
+		return "U+{0:04X}".format(u)
 	else:
 		return "No USV" #length same as above
 
@@ -38,10 +28,11 @@ args = len(sys.argv)
 if args>1:
 	fn = sys.argv[1]
 else:
+	# urrgh, we'll deal with this later when we do argument parsing
 	fn = "/home/david/RFS/testdata/GenBasR.sfd"
 
 (fpath,fontn,fext) = splitfn(fn)
-fn = fpath+fontn+'.sfd'
+fn = os.path.join(fpath, fontn+'.sfd')  # to be precise os.path.join(fpath, fontn + os.path.extsep + 'sfd')
 print 'Opening ' + fn
 font = fontforge.open(fn)
 
@@ -49,16 +40,10 @@ font = fontforge.open(fn)
 if args>2:
 	fn = sys.argv[2]
 else:
-	fn = fpath+fontn+'DblEnc'+'.txt'
+	fn = os.path.join(fpath, fontn+'DblEnc'+'.txt')
 
 (opath,obase,oext)=splitfn(fn)
-if opath == '':
-	opath=fpath
-if obase == '':
-	obase = font
-if oext == '':
-	oext = '.txt'
-fn = opath+obase+oext
+fn = os.path.join(opath or fpath, (obase or fbase) + (oext or '.txt'))
 print 'Opening ' + fn
 inpf = open(fn, "r")
 
@@ -70,33 +55,22 @@ else:
 	fn = fpath+fontn+'DblEnc'+'.log'
 
 (opath,obase,oext)=splitfn(fn)
-if opath == '':
-	opath=fpath
-if obase == '':
-	obase = font
-if oext == '':
-	oext = '.log'
+fn = os.path.join(opath or fpath, (obase or fbase) + (oext or '.log'))
 fn = opath+obase+oext
 print 'Opening ' + fn
 logf = open(fn, "w")
 
-# Read the lines from the input file
-lines = inpf.readlines()
-inpf.close()
-
 # Create dbl_encode list from lines
 dbl_encode = {}
-for line in lines:
-	fields = line.strip().split(",") #this should be safe given the format of the DblEnc file
-	assert(len(fields) == 3)
-	glyphn, pua_usv_str, std_usv_str = fields[0], fields[1], fields[2]
-	glyphn = glyphn[1:-1] #slice off quote marks
+for line in inpf.readlines() :
+	glyphn, pua_usv_str, std_usv_str = line.strip().split(",")  # will exception if not 3 elements
+	if glyphn[0] in '"\'' : glyphn = glyphn[1:-1]               # slice off quote marks, if present
 	pua_usv, std_usv = int(pua_usv_str[2:], 16), int(std_usv_str[2:], 16)
 	dbl_encode[glyphn] = [std_usv, pua_usv]
+inpf.close()
 
-glyphs = dbl_encode.keys()
-glyphs.sort()
-for glyph in glyphs:
+for glyph in sorted(dbl_encode.keys()) :
+	if glyph not in font : continue         # ignore missing glyphs (assuming you want this)
 	g = font[glyph]
 	ousvs=[g.unicode]
 	oalt=g.altuni
@@ -107,7 +81,7 @@ for glyph in glyphs:
 			ousvs.append(au[0]) # (may need to check variant flag)
 	dbl = dbl_encode[glyph]
 	g.unicode = dbl[0]
-	g.altuni = ((dbl[1],),)
+	g.altuni = ((dbl[1],),)     # maybe easier as a list [[dbl[1]]] if it works
 	logf.write("encoding for %s changed: %s -> %s\n" % (glyph, ousvs, dbl))
 	# fl.UpdateGlyph(glyph_ix)
 
