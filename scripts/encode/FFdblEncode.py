0f236eca01d1396119c5659824e7e4cb29736bbc
Martin Hosken <martin_hosken@sil.org>
2013-10-03 12:08:48 +0700

    Add fontforge based framework
    This hides all the boilerplate code in a module.
    Make friends with the argparse module before diving into this.
    Hope you like the new simplicity.
    This is all very very untested.

--- scripts/encode/FFdblEncode.py
+++ scripts/encode/FFdblEncode.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 #
-# FFdblEncode.py
-# FontForge: Double encode all glyphs based on double encoding data in a file
-# Lines in file should look like: "LtnSmARetrHook",U+F236,U+1D8F
-# usage: python FFdblEncode.py [sourcefont.sfd] [input file name] [output file name]
-# from http://projects.palaso.org/projects/pysilfont
-#
-# Copyright (c) 2013, SIL International  (http://www.sil.org)
-# Released under the MIT License (http://sil.mit-license.org)
+'''Double encode all glyphs based on double encoding data in a file
+Lines in file should look like: "LtnSmARetrHook",U+F236,U+1D8F'''
+__version__ = '0.0.1'
+__menuentry__ = 'FFdblEncode'
+__url__ = 'http://projects.palaso.org/projects/pysilfont'
+__copyright__ = '''
+Copyright (c) 2013, SIL International  (http://www.sil.org)
+Released under the MIT License (http://sil.mit-license.org)
+'''
 
 import fontforge, sys, string, os.path
-
-def splitfn(fn): # Split filename into path, base and extension
-	p = os.path.split(fn)
-	f = os.path.splitext(p[1])
-	return (p[0], f[0], os.path.extsep + f[1])
+from silfont.fontforge.framework import execute
 
 def UniStr(u):
 	if u:
@@ -22,71 +19,43 @@ def UniStr(u):
 	else:
 		return "No USV" #length same as above
 
-args = len(sys.argv)
-
-# Open the font
-if args>1:
-	fn = sys.argv[1]
-else:
-	# urrgh, we'll deal with this later when we do argument parsing
-	fn = "/home/david/RFS/testdata/GenBasR.sfd"
-
-(fpath,fontn,fext) = splitfn(fn)
-fn = os.path.join(fpath, fontn+'.sfd')  # to be precise os.path.join(fpath, fontn + os.path.extsep + 'sfd')
-print 'Opening ' + fn
-font = fontforge.open(fn)
-
-# Open the input file
-if args>2:
-	fn = sys.argv[2]
-else:
-	fn = os.path.join(fpath, fontn+'DblEnc'+'.txt')
-
-(opath,obase,oext)=splitfn(fn)
-fn = os.path.join(opath or fpath, (obase or fbase) + (oext or '.txt'))
-print 'Opening ' + fn
-inpf = open(fn, "r")
-
-
-# Open the log file
-if args>3:
-	fn = sys.argv[3]
-else:
-	fn = fpath+fontn+'DblEnc'+'.log'
-
-(opath,obase,oext)=splitfn(fn)
-fn = os.path.join(opath or fpath, (obase or fbase) + (oext or '.log'))
-fn = opath+obase+oext
-print 'Opening ' + fn
-logf = open(fn, "w")
-
+def doit(font, args) :
+	if not args.input : args.input = args.infont.replace('.sfd', 'DblEnc.txt')
+	if not args.log : args.log = args.infont.replace('.sfd', 'DblEnc.log')
+	inpf = open(args.input, 'r')
+	logf = open(args.log, 'w')
 # Create dbl_encode list from lines
-dbl_encode = {}
-for line in inpf.readlines() :
-	glyphn, pua_usv_str, std_usv_str = line.strip().split(",")  # will exception if not 3 elements
-	if glyphn[0] in '"\'' : glyphn = glyphn[1:-1]               # slice off quote marks, if present
-	pua_usv, std_usv = int(pua_usv_str[2:], 16), int(std_usv_str[2:], 16)
-	dbl_encode[glyphn] = [std_usv, pua_usv]
-inpf.close()
+	dbl_encode = {}
+	for line in inpf.readlines() :
+		glyphn, pua_usv_str, std_usv_str = line.strip().split(",")  # will exception if not 3 elements
+		if glyphn[0] in '"\'' : glyphn = glyphn[1:-1]               # slice off quote marks, if present
+		pua_usv, std_usv = int(pua_usv_str[2:], 16), int(std_usv_str[2:], 16)
+		dbl_encode[glyphn] = [std_usv, pua_usv]
+	inpf.close()
 
-for glyph in sorted(dbl_encode.keys()) :
-	if glyph not in font : continue         # ignore missing glyphs (assuming you want this)
-	g = font[glyph]
-	ousvs=[g.unicode]
-	oalt=g.altuni
-	if oalt != None:
-		print oalt
-		for au in oalt:
-			print au
-			ousvs.append(au[0]) # (may need to check variant flag)
-	dbl = dbl_encode[glyph]
-	g.unicode = dbl[0]
-	g.altuni = ((dbl[1],),)     # maybe easier as a list [[dbl[1]]] if it works
-	logf.write("encoding for %s changed: %s -> %s\n" % (glyph, ousvs, dbl))
-	# fl.UpdateGlyph(glyph_ix)
+	for glyph in sorted(dbl_encode.keys()) :
+		if glyph not in font : continue         # ignore missing glyphs (assuming you want this)
+		g = font[glyph]
+		ousvs=[g.unicode]
+		oalt=g.altuni
+		if oalt != None:
+			print oalt
+			for au in oalt:
+				print au
+				ousvs.append(au[0]) # (may need to check variant flag)
+		dbl = dbl_encode[glyph]
+		g.unicode = dbl[0]
+		g.altuni = ((dbl[1],),)     # maybe easier as a list [[dbl[1]]] if it works
+		logf.write("encoding for %s changed: %s -> %s\n" % (glyph, ousvs, dbl))
+	logf.close()
+	return font
 
-# fl.UpdateFont()
-font.save()
-logf.close()
+#execute(doit, options = [
+#    ('input', {'nargs': '?', 'help': 'Input CSV text file'}),
+#    ('log', {'nargs' : '?', 'help': 'Log file to output'})])
 
-print "done"
+execute(doit, options = [
+	('-i','--input',{'help': 'Input CSV text file'}),
+	('-o','--output',{'dest': 'outfont', 'help': 'Output the font here'}),
+	('-l','--log',{'help': 'Lgo file to output'})])
+
